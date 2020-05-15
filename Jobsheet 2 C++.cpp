70c93cea9b0302f83b0e696484cded75e2c64845
RahmatRiyadiYearham <64615792+RahmatRiyadiYearham@users.noreply.github.com>
2020-05-16 05:26:39 +0800

    Create Jobsheet 2 C++.cpp
    Mengisi dan Menampilkan Elemen Aray(Dinamis)

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	cout<<"nama:RAHMAT RIYADI YEARHAM"<<endl;
+	cout<<"nim:F1B019118"<<endl;
+	cout<<"kelompok:24"<<endl;
+
+int x [6];
+cout<<"masukkan 2 angka NIM terakhir"<<endl;
+cout<<"masukan nim 1 : ";
+cin>>x[0];
+cout<<"masukan nim 2 : ";
+cin>>x[1];
+cout<<"masukan nim 3 : ";
+cin>>x[2];
+cout<<"masukan nim 4 : ";
+cin>>x[3];
+cout<<"masukan nim 5 : ";
+cin>>x[4];
+cout<<"masukan nim 6 : ";
+cin>>x[5];
+cout<<"masukan nim 7 : ";
+cin>>x[6];
+cout<<"masukan nim 8 : ";
+cin>>x[7];
+cout<<"masukan nim 9 : ";
+cin>>x[8];
+cout<<"masukan nim 10 : ";
+cin>>x[9];
+cout<<"masukan nim 11 : ";
+cin>>x[10];
+cout<<"masukan nim 12 : ";
+cin>>x[11];
+cout<<"masukan nim 13 : ";
+cin>>x[12];
+return 0 ;
+}
