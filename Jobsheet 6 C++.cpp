3413c053ea4327e7097ac8d05c83e7bedc77678e
RahmatRiyadiYearham_F1B019118_klp24 <64615792+RahmatRiyadiYearham@users.noreply.github.com>
2020-05-16 05:45:27 +0800

    Create Jobsheet 6 C++.cpp
    Matriks transpose

---
+++ Jobsheet 6 C++.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+main (){
+	cout<<"nama:RAHMAT RIYADI YEARHAM"<<endl;
+	cout<<"nim:F1B019118"<<endl;
+	cout<<"kelompok:24"<<endl;
+	
+int x,y, mtrx [5][5];
+cout<< "TRANSPOSE MATRIX" <<endl;
+cout<< "Masukkan jumlahbaris = ";
+cin>> x;
+cout<< "Masukkan jumlahkolom = ";
+cin>> y;
+cout<<endl;
+for (int i = 0; i< x; i++){
+for (int j = 0; j < y; j++){
+cout<< "Masukkan nilai index " <<i<< "," << j << " = ";
+cin>>mtrx [i][j];
+}
+}
+cout<<endl;
+for (int i = 0; i< x; i++){
+for (int j = 0; j < y; j++){
+cout<<mtrx [i][j] <<"  ";
+}
+cout<<endl<<endl;
+}
+cout<<endl;
+for (int i = 0; i< y; i++){
+for (int j = 0; j < x; j++){
+cout<<mtrx [j][i] <<"  ";
+}
+cout<<endl<<endl;
+}
+}
