6db03f334843997eadcdfc16c8984daf0744355d
RahmatRiyadiYearham <64615792+RahmatRiyadiYearham@users.noreply.github.com>
2020-05-16 05:23:32 +0800

    Create Jobsheet 1 C++.cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"NAMA 	: RAHMAT RIYADI YEARHAM\n\n";
+	cout<<"NIM  	: F1B019118\n\n";
+	cout<<"KELOMPOK : 24\n\n";
+int nim[5]= {8,11,9,8,7};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+}
